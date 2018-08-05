c34023eb00e01daea0a025bde3777e6ef138d2e4
Subhajit Sahu <wolfram77@gmail.com>
2018-08-05 18:17:30 +0530

    :snowman: corpus is available to browsers

---
+++ corpus.js
@@ -0,0 +1,6 @@
+exports.columns = require('@ifct2017/columns/corpus');
+exports.hierarcy = require('@ifct2017/hierarchy/corpus');
+exports.intakes = require('@ifct2017/intakes/corpus');
+exports.methods = require('@ifct2017/methods/corpus');
+exports.nutrients = require('@ifct2017/nutrients/corpus');
+exports.representations = require('@ifct2017/representations/corpus');
