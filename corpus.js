f40d6ea68221e7320f4425e8bf9de58472763004
Subhajit Sahu <wolfram77@gmail.com>
2018-08-07 16:18:49 +0530

    :snowman: corpus: exports.hierarcy -> hierarchy

--- corpus.js
+++ corpus.js
@@ -1,5 +1,5 @@
 exports.columns = require('@ifct2017/columns/corpus');
-exports.hierarcy = require('@ifct2017/hierarchy/corpus');
+exports.hierarchy = require('@ifct2017/hierarchy/corpus');
 exports.intakes = require('@ifct2017/intakes/corpus');
 exports.methods = require('@ifct2017/methods/corpus');
 exports.nutrients = require('@ifct2017/nutrients/corpus');
